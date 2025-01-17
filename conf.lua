28e805fa864071d9688694199270a848241f3b7e
fivoA <fivo.ahmed23@gmail.com>
2025-01-17 14:56:51 +0100

    game files

---
+++ conf.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.window.title = "Kelly's Last Days"
+    t.window.icon = "assets/images/menuBG.png"
+    t.window.resizable = true
+    t.window.borderless = false
+    t.window.width = 800
+    t.window.height = 800
+    t.console=true
+    t.modules.joystick = false
+    t.modules.physics = false
+end
