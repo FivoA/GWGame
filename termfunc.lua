bfb3ec45d4a9535bfba49181fa2bd0c1a1bcc76d
Fivo Ahmed <>
2025-01-28 10:57:37 +0100

    .

--- termfunc.lua
+++ termfunc.lua
@@ -222,7 +222,7 @@ function Termfunc.report(terminal)
     terminal:println("Fetching citizen id...")
     terminal:println("Report created for citizen with id Citizen#3857")
     if day == 1 then
-        dayswitch = true
+        switchDay = true
     end
 end
 
